b0f0a8e12314da14c56e5aa8a47a0d5fc96ecd88
Stephen Mathieson <smath23@gmail.com>
2013-06-27 19:15:48 -0400

    seperating RegExp checks

--- test/unit/proclaim.js
+++ test/unit/proclaim.js
@@ -196,16 +196,25 @@
             });
 
             it('should handle RegExps', function () {
-                assert.throws(function () {
-                    var a = new RegExp('goodbye', 'g'),
-                        b = /goodbye/gi,
-                        c = new RegExp('hello', 'g'),
-                        d = /hello/i;
+                var a = new RegExp('goodbye', 'g'),
+                    b = /goodbye/gi,
+                    c = new RegExp('hello', 'g'),
+                    d = /hello/i,
+                    e = new RegExp('hello', 'i');
 
+                assert.doesNotThrow(function () {
                     proclaim.deepEqual(a, a);
+                });
+
+                assert.throws(function () {
                     proclaim.deepEqual(a, b);
                     proclaim.deepEqual(a, c);
                     proclaim.deepEqual(a, d);
+                    proclaim.deepEqual(a, e);
+                });
+
+                assert.doesNotThrow(function () {
+                    proclaim.deepEqual(d, e);
                 });
             });
         });
