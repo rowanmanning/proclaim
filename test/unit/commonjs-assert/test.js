4289d503a79d7ef8dc1a4e5b1d1a81828ca21b9a
Jake Champion <jakechampion.jake2@googlemail.com>
2017-10-03 11:38:39 +0100

    Fix linting issues

--- test/unit/commonjs-assert/test.js
+++ test/unit/commonjs-assert/test.js
@@ -1,9 +1,10 @@
+'use strict';
 var commonjsUnitTestingUnitTests = require('./program')(require('../../../lib/proclaim'));
 
 describe('commonjs unit testing unit tests converted to mocha', function () {
-	for (var test in commonjsUnitTestingUnitTests) {
-		if (Object.prototype.hasOwnProperty.call(commonjsUnitTestingUnitTests, test)) {
-			it(test, commonjsUnitTestingUnitTests[test]);
-		}
-	}
+    for (var test in commonjsUnitTestingUnitTests) {
+        if (Object.prototype.hasOwnProperty.call(commonjsUnitTestingUnitTests, test)) {
+            it(test, commonjsUnitTestingUnitTests[test]);
+        }
+    }
 });
