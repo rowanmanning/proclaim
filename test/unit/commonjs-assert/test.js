9c80794476eb85ba846259d8df0a4163e2a96e59
Jake Champion <jakechampion.jake2@googlemail.com>
2017-10-03 12:44:03 +0100

    switch to newer commonjs assert tests

--- test/unit/commonjs-assert/test.js
+++ test/unit/commonjs-assert/test.js
@@ -1,10 +1,6 @@
 'use strict';
-var commonjsUnitTestingUnitTests = require('./program')(require('../../../lib/proclaim'));
+var commonjsUnitTestingUnitTests = require('./program');
 
 describe('commonjs unit testing unit tests converted to mocha', function () {
-    for (var test in commonjsUnitTestingUnitTests) {
-        if (Object.prototype.hasOwnProperty.call(commonjsUnitTestingUnitTests, test)) {
-            it(test, commonjsUnitTestingUnitTests[test]);
-        }
-    }
+    commonjsUnitTestingUnitTests(require('../../../lib/proclaim'));
 });
